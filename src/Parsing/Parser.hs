5cf1530f3c59481375c6994939c25dd228e7679f
Ben Price <daebr.77@gmail.com>
2020-08-04 13:40:04 +1000

    add string parser and basic combinators

--- src/Parsing/Parser.hs
+++ src/Parsing/Parser.hs
@@ -1,16 +1,32 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, FlexibleInstances #-}
 
 module Parsing.Parser
     ( Parser
     , ParseResult
+    , (.&&.)
+    , (.&&)
+    , (&&.)
     , parse
+    , option
     , pchar
+    , pdigit
     ) where
 
+import Data.Char (isDigit)
+import Control.Applicative (Alternative(..))
+import Control.Monad (MonadPlus)
 import Control.Monad.Trans.State.Lazy (StateT(..), runStateT)
 
 type Parser a = StateT String (Either String) a
 type ParseResult a = Either String a
+
+instance Alternative (Either String) where
+    empty = Left "no alternative match"
+    (Right a) <|> _ = Right a
+    _ <|> (Right a) = Right a
+    _ <|> _ = empty
+
+instance MonadPlus (Either String)
 
 lift :: (String -> ParseResult (a, String)) -> Parser a
 lift = StateT 
@@ -18,11 +34,29 @@ lift = StateT
 parse :: Parser a -> String -> ParseResult (a, String)
 parse = runStateT
 
+infixl 5 .&&.
+(.&&.) :: Parser a -> Parser b -> Parser (a, b)
+pa .&&. pb = (,) <$> pa <*> pb
+
+infixl 5 .&&
+(.&&) :: Parser a -> Parser b -> Parser a
+pa .&& pb = fst <$> pa .&&. pb
+
+infixl 5 &&.
+(&&.) :: Parser a -> Parser b -> Parser b
+pa &&. pb = snd <$> pa .&&. pb
+
 match :: (Char -> Bool) -> Parser Char
 match f = lift $ \case
     [] -> Left "empty"
     (x:xs) | f x -> Right (x, xs)
            | otherwise -> Left "parse failed"
 
+option :: Parser a -> Parser (Maybe a)
+option p = Just <$> p <|> pure Nothing
+
 pchar :: Char -> Parser Char
 pchar = match . (==)
+
+pdigit :: Parser Char
+pdigit = match isDigit
--- test/Parsing/ParserTest.hs
+++ test/Parsing/ParserTest.hs
@@ -1,5 +1,6 @@
 module Parsing.ParserTest where
 
+import Control.Applicative ((<|>))
 import Data.Either (isLeft)
 import Test.HUnit
 
@@ -8,6 +9,11 @@ import Parsing.Parser
 suite :: Test
 suite = TestLabel "Parser" (TestList
     [ pcharTest
+    , pdigitTest
+    , altTest
+    , tupleTest
+    , ignoreTest
+    , optionTest
     ])
 
 pcharTest :: Test
@@ -15,4 +21,39 @@ pcharTest = TestLabel "pchar" (TestList
     [ TestCase $ assertEqual "match" (Right ('a', "bc")) (parse (pchar 'a') "abc")
     , TestCase $ assertBool "not match" $ isLeft (parse (pchar 'b') "abc")
     , TestCase $ assertBool "empty" $ isLeft (parse (pchar 'a') "")
+    ])
+
+pdigitTest :: Test
+pdigitTest = TestLabel "pdigit" (TestList
+    [ TestCase $ assertEqual "match" (Right ('1', "")) $ parse pdigit "1"
+    , TestCase $ assertBool "not match" $ isLeft (parse pdigit "a")
+    ])
+
+altTest :: Test
+altTest = TestLabel "<|>" (TestList
+    [ TestCase $ assertEqual "Right <|> Left" (Right ('a', "bc")) $ parse (pchar 'a' <|> pchar 'b') "abc"
+    , TestCase $ assertEqual "Left <|> Right" (Right ('b', "c")) $ parse (pchar 'a' <|> pchar 'b') "bc"
+    , TestCase $ assertBool "Left <|> Left" $ isLeft (parse (pchar 'a' <|> pchar 'b') "c")
+    ])
+
+tupleTest :: Test
+tupleTest = TestLabel ".&&." (TestList
+    [ TestCase $ assertEqual "success" (Right (('a','b'),"c")) $ parse (pchar 'a' .&&. pchar 'b') "abc"
+    , TestCase $ assertBool "left fail" $ isLeft (parse (pchar 'a' .&&. pchar 'b') "bab")
+    , TestCase $ assertBool "right fail" $ isLeft (parse (pchar 'a' .&&. pchar 'b') "acb")
+    , TestCase $ assertBool "empty" $ isLeft (parse (pchar 'a' .&&. pchar 'b') "a")
+    ])
+
+ignoreTest :: Test
+ignoreTest = TestLabel "ignore" (TestList
+    [ TestCase $ assertEqual "a &&. b" (Right ('b', "c")) $ parse (pchar 'a' &&. pchar 'b') "abc"
+    , TestCase $ assertEqual "a .&& b" (Right ('a', "c")) $ parse (pchar 'a' .&& pchar 'b') "abc"
+    , TestCase $ assertBool "! &&. b" $ isLeft (parse (pchar 'b' &&. pchar 'a') "abc")
+    , TestCase $ assertBool "a .&& !" $ isLeft (parse (pchar 'a' .&& pchar 'c') "abc")
+    ])
+
+optionTest :: Test
+optionTest = TestLabel "option" (TestList
+    [ TestCase $ assertEqual "match" (Right (Just 'a', "bc")) $ parse (option $ pchar 'a') "abc"
+    , TestCase $ assertEqual "no match" (Right (Nothing, "abc")) $ parse (option $ pchar 'b') "abc"
     ])
