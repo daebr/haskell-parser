ca106c8784213321740afa9504408f9931d3991e
Ben Price <daebr.77@gmail.com>
2020-08-16 22:19:51 +1000

    progress - add attribute parsing

--- src/Parsing/Xml/XmlParser.hs
+++ src/Parsing/Xml/XmlParser.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Parsing.Xml.XmlParser
     ( XDocument(..)
     , XNode(..)
     , XElement(..)
+    , XAttr(..)
     , XName(..)
     , XValue(..)
     , xmlParser
@@ -11,18 +14,27 @@ module Parsing.Xml.XmlParser
     ) where
 
 import Data.Function ((&))
+import Data.List (intersperse)
 import Data.List.Extra (trim)
 
 import Parsing.Parser
 
+class Xml a where
+    toXml :: a -> String
 
 type XValue = String
+
 newtype XName = XName String deriving (Eq, Show)
-data XElement = XElement XName XNode deriving (Eq, Show)
+
+data XAttr = XAttr XName XValue deriving (Eq, Show)
+
+data XElement = XElement XName [XAttr] XNode deriving (Eq, Show)
+
 data XNode
-    = Element XElement
+    = Elements [XElement]
     | Value XValue
   deriving (Eq, Show)
+
 newtype XDocument = XDocument { root :: XElement } deriving (Eq, Show)
 
 xmlParser :: Parser XDocument
@@ -44,29 +56,61 @@ anyWhitespace = zeroOrMore whitespace
 openingTag :: Parser XName
 openingTag = (pchar '<') &&. pname .&& (pchar '>')
 
-closingTag :: XName -> Parser XName
-closingTag n = (pstr "</") &&. (pfilter (== n) pname) .&& (pstr ">")
-
 pvalue :: Parser XValue
 pvalue = fmap trim . zeroOrMore $ pfilter (not . flip elem disallowedChars) panychar
   where
     disallowedChars = [ '<', '>', '\r', '\n' ]
 
 pelement :: Parser XElement
-pelement = pinlineElement <|> ptaggedElement
+pelement = ptaggedElement
 
 ptaggedElement :: Parser XElement
 ptaggedElement = XElement 
-                 <$> openingTag
+                 <$> (pchar '<') &&. pname .&& (pchar '>')
+                 <*> pure [] 
                  <*> anyWhitespace &&. pnode .&& anyWhitespace
-                 >>= \(XElement n v) -> pure (XElement n v) .&& anyWhitespace .&& closingTag n .&& anyWhitespace
+                 >>= \(XElement name attrs node) -> pure (XElement name attrs node) .&& anyWhitespace .&& closingTag name .&& anyWhitespace
+  where
+    closingTag :: XName -> Parser XName
+    closingTag n = (pstr "</") &&. (pfilter (== n) pname) .&& (pstr ">")
+                   -- & withError "Closing tag expected for '" <> toXml n <> "'"
 
 pinlineElement :: Parser XElement
-pinlineElement = XElement <$> (pstr "<") &&. pname .&& (pstr " />") <*> noValue
+pinlineElement = XElement <$> (pstr "<") &&. pname .&& (pstr " />") <*> pure [] <*> noValue
   where
     noValue :: Parser XNode
     noValue = pure $ Value ""
 
 pnode :: Parser XNode
-pnode = anyWhitespace &&. (Element <$> pelement <|> Value <$> pvalue) .&& anyWhitespace
+pnode = Elements <$> pelementList <|> Value <$> pvalue
         & withError "Invalid node"
+  where
+    pelementList :: Parser [XElement]
+    pelementList = zeroOrMore (pelement .&& anyWhitespace)
+
+instance Xml XValue where
+    toXml s = s
+
+instance Xml XName where
+    toXml (XName n) = n
+
+instance Xml XAttr where
+    toXml (XAttr n v) = toXml n <> "=\"" <> v <> "\""
+
+instance Xml XElement where
+    toXml (XElement n attrs node) = case node of
+        Value "" -> "<" <> toXml n <> " " <> attrString <> " />"
+        otherwise ->
+            unlines [ "<" <> toXml n <> " " <> attrString <> ">"
+                    , toXml node
+                    , "</" <> toXml n <> ">"
+                    ]
+      where
+        attrString = mconcat . intersperse " " $ toXml <$> attrs
+
+instance Xml XNode where
+    toXml (Elements es) = unlines $ toXml <$> es
+    toXml (Value v) = v
+
+instance Xml XDocument where
+    toXml = toXml . root
--- test/Parsing/Xml/XmlParserTest.hs
+++ test/Parsing/Xml/XmlParserTest.hs
@@ -20,18 +20,25 @@ evalP p = bimap message fst . parse p
 evalPS :: Parser a -> String -> Either String a
 evalPS p = bimap message fst . parseString p
 
-xelement :: String -> String -> XElement
-xelement n v = XElement (XName n) (Value v)
+xelement :: String -> [XAttr] -> String -> XElement
+xelement n as v = XElement (XName n) as (Value v)
+
+xattr :: String -> String -> XAttr
+xattr n = XAttr (XName n)
 
 pelementTest :: Test
 pelementTest = TestLabel "pelement" (TestList
-    [ TestCase $ assertEqual "standard tags" (Right $ xelement "name" "value") $ evalPS pelement "<name>value</name>"
-    , TestCase $ assertEqual "inline tag" (Right $ xelement "name" "") $ evalPS pelement "<name />"
-    , TestCase $ assertEqual "empty tag" (Right $ xelement "name" "") $ evalPS pelement "<name></name>"
+    [ TestCase $ assertEqual "standard tags" (Right $ xelement "name" [] "value") $ evalPS pelement "<name>value</name>"
+    , TestCase $ assertEqual "inline tag" (Right $ xelement "name" [] "") $ evalPS pelement "<name />"
+    , TestCase $ assertEqual "empty tag" (Right $ xelement "name" [] "") $ evalPS pelement "<name></name>"
+    , TestCase $ assertEqual "standard w/ attrs" (Right $ xelement "name" [xattr "attr1" "1", xattr "attr2" "2"] "") $ evalPS pelement "<name attr1=\"1\" attr2=\"2\"></name>"
+    , TestCase $ assertEqual "inline w/ attrs" (Right $ xelement "name" [xattr "attr1" "1", xattr "attr2" "2"] "") $ evalPS pelement "<name attr1=\"1\" attr2=\"2\" />"
     , TestCase $ assertBool "no end tag" $ isLeft (evalPS pelement "<name>value</othername>")
     , TestCase $ assertBool "no end inline" $ isLeft (evalPS pelement "<name /")
     , TestCase $ assertBool "no slash inline" $ isLeft (evalPS pelement "<name >")
     , TestCase $ assertBool "empty tag" $ isLeft (evalPS pelement "<>value</>")
+    , TestCase $ assertBool "no closing attr quote" $ isLeft (evalPS pelement "<name attr=\"1 />")
+    , TestCase $ assertBool "no attr quote" $ isLeft (evalPS pelement "<name attr=1 />")
     ])
 
 integrationTest :: Test
@@ -40,15 +47,24 @@ integrationTest = TestCase $ assertEqual "integration" (Right expected) $ evalP 
     xml =
         [ "<root>"
         , "   <items>"
-        , "       <item>"
-        , "           value"
+        , "       <item id=\"1\">"
+        , "           value 1"
+        , "       </item>"
+        , "       <item id=\"2\">"
+        , "           value 2"
         , "       </item>"
         , "   </items>"
         , "</root>"
         ]
     expected = XDocument
-        ( XElement (XName "root")
-            ( Element $ XElement (XName "items")
-                ( Element $ XElement (XName "item") (Value "value") )
+        ( XElement (XName "root") []
+            ( Elements 
+                [ XElement (XName "items") []
+                    ( Elements
+                        [ XElement (XName "item") [XAttr (XName "id") "1"] (Value "value 1")
+                        , XElement (XName "item") [XAttr (XName "id") "2"] (Value "value 2")
+                        ]
+                    )
+                ]
             )
         )
