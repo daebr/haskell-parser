f5ca0f3e06620443ded7216b4e325cdf3254e274
Ben Price <daebr.77@gmail.com>
2022-11-25 09:14:01 +1100

    parameterise input and error

--- src/Parsing/Parser.hs
+++ src/Parsing/Parser.hs
@@ -1,38 +1,39 @@
 {-# LANGUAGE LambdaCase, FlexibleInstances #-}
 
 module Parsing.Parser
-    ( Parser
-    , ParseResult
-    , (.&&.)
-    , (.&&)
-    , (&&.)
+    ( Parser()
+    , EndOfInputError(..)
+    , MatchError(..)
     , (<?>)
-    , parse
-    , parseString
-    , withError
-    , failWith
-    , option
-    , zeroOrMore
-    , oneOrMore
+    , (<||>)
     , anyOf
-    , pfilter
-    , pchar
-    , pcharIn
-    , pcharNotIn
-    , pdigit
-    , pint
-    , pstr
-    , pquotedstr
-    , whitespace
-    , eol
-    , eof
+    , anyOfNE
+    , eval
+    , exec
+    , failIf
+    , failure
+    , filterWith
+    , lift
+    , mapError
+    , match
+    , oneOrMore
+    , option
+    , orElse
+    , parse
+    , pelem
+    , pend
+    , run
+    , setError
+    , zeroOrMore
     ) where
 
-import Data.Bifunctor (first)
+import Data.Bifunctor (Bifunctor(..))
 import Data.Bool (bool)
 import Data.Char (isDigit)
 import Data.Functor (($>), (<&>))
+import Data.List.NonEmpty (NonEmpty(..))
 import Data.Maybe (isJust)
+import Data.Void (Void)
 import Control.Applicative (Alternative(..))
 import Control.Monad (MonadPlus)
 import Control.Monad.Trans.State.Lazy (StateT(..), runStateT)
@@ -43,131 +44,123 @@ import Parsing.ParseState (ParseState(..), Source, curLine, curCol)
 import Parsing.Position (Position)
 import qualified Parsing.Position as Pos
 
-type Parser a = StateT ParseState (Either ParseError) a
-type ParseResult a = Either ParseError (a, ParseState)
+newtype Parser i e a = Parser { runParse :: i -> Either e (a, i) }
 
-instance Alternative (Either ParseError) where
-    empty = Left (ParseError "Unknown failure" "" Pos.new)
+lift :: (i -> Either e (a, i)) -> Parser i e a
+lift = Parser
 
-    (Right a) <|> _ = Right a
-    _ <|> (Right a) = Right a
-    Left e <|> _ = Left e
+run :: Parser i e a -> i -> Either e (a, i)
+run = runParse
 
-instance MonadPlus (Either ParseError)
+parse :: Parser i e a -> i -> Either e a
+parse p = fmap fst . run p
 
-lift :: (ParseState -> ParseResult a) -> Parser a
-lift = StateT 
+exec :: Parser i e a -> i -> Either e a
+exec = parse
 
-parse :: Parser a -> Source -> ParseResult a
-parse p = runStateT p . ParseState.new
+eval :: Parser i e a -> i -> Either e i
+eval p = fmap snd . run p
 
-parseString :: Parser a -> String -> ParseResult a
-parseString p = parse p . lines
+instance Functor (Parser i e) where
+    fmap f p = lift $ fmap (first f) . run p
 
-toError :: String -> ParseState -> ParseError
-toError msg s = ParseError msg curLine (position s)
+instance Bifunctor (Parser i) where
+    bimap f g p = lift $ bimap f (first g) . run p
+
+instance Applicative (Parser i e) where
+    pure a = lift $ \i -> Right (a, i)
+    pf <*> pa = lift $ \i ->
+        case run pf i of
+            Right (f, i') -> first f <$> run pa i'
+            Left e -> Left e
+
+instance Monad (Parser i e) where
+    pa >>= f = lift $ \i ->
+        case run pa i of
+            Right (a, i') -> run (f a) i'
+            Left e -> Left e
+
+failure :: e -> Parser i e a
+failure e = lift (const $ Left e)
+
+failIf :: (a -> e) -> (a -> Bool) -> Parser i e a -> Parser i e a
+failIf fe f p = lift $ (>>= handle) . run p
   where
-    curLine = case source s of
-        [] -> ""
-        (x:_) -> x
+    handle (a,i) 
+        | f a = Right (a,i)
+        | otherwise = Left $ fe a
 
-next :: Parser Char
-next = lift doNext
-  where
-    doNext :: ParseState -> ParseResult Char
-    doNext s = case source s of
-        [] -> Left $ toError "EOF" s
-        (x:xs) | (curCol s) >= length x -> Right ('\n', s { source=xs, position=(Pos.nextLine $ position s) })
-               | otherwise -> Right(x !! curCol s, s { position=(Pos.nextCol $ position s) })
+mapError :: (e -> e') -> Parser i e a -> Parser i e' a
+mapError f p = lift $ first f . run p
 
-failWith :: String -> Parser a
-failWith s = lift $ Left . toError s 
-
-withError :: String -> Parser a -> Parser a
-withError msg p = lift $ \s ->
-    case runStateT p s of
-        Left err -> Left $ err { message=msg }
-        Right a -> Right a 
+setError :: e' -> Parser i e a -> Parser i e' a
+setError e = mapError $ const e
 
 infixl 3 <?>
-(<?>) :: Parser a -> String -> Parser a
-p <?> s = withError s p
+(<?>) :: Parser i e a -> (e -> e') -> Parser i e' a
+(<?>) = flip mapError
 
-infixl 5 .&&.
-(.&&.) :: Parser a -> Parser b -> Parser (a, b)
-pa .&&. pb = (,) <$> pa <*> pb
+data MatchError
+    = NoInput
+    | MatchFailure
+  deriving
+    (Eq, Show)
 
-infixl 5 .&&
-(.&&) :: Parser a -> Parser b -> Parser a
-pa .&& pb = fst <$> pa .&&. pb
+match :: (a -> Bool) -> Parser [a] MatchError a
+match f = lift $ \i ->
+    case i of
+        [] -> Left NoInput
+        (x:xs)
+            | f x -> Right (x, xs)
+            | otherwise -> Left MatchFailure
 
-infixl 5 &&.
-(&&.) :: Parser a -> Parser b -> Parser b
-pa &&. pb = snd <$> pa .&&. pb
+data EndOfInputError = ExpectedEndOfInput deriving (Eq, Show)
 
-zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = oneOrMore p <|> pure []
+pend :: Parser [a] EndOfInputError ()
+pend = lift p
+  where
+    p [] = Right ((), [])
+    p xs = Left ExpectedEndOfInput
 
-oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = (:) <$> p <*> (oneOrMore p <|> pure [])
+orElse :: Parser i e a -> Parser i e a -> Parser i e a
+orElse p1 p2 = lift $ \input ->
+    case run p1 input of
+        Right a -> Right a
+        Left e -> case run p2 input of
+            Right a -> Right a
+            Left _ -> Left e
 
-anyOf :: [Parser a] -> Parser a
-anyOf = foldl (<|>) (failWith "No parser satisfied")
+infixl 3 <||>
+(<||>) = orElse
 
-pfilter :: (a -> Bool) -> Parser a -> Parser a
-pfilter f p = lift $ \s ->
-    case runStateT p s of
-        Right (a, s') | f a -> Right (a, s')
-                      | otherwise ->  Left $ toError "Failed to satisfy filter" s
+anyOf :: e' -> [Parser i e a] -> Parser i e' a
+anyOf err = foldl (<||>) (failure err) . fmap (setError err)
+
+anyOfNE :: NonEmpty (Parser i e a) -> Parser i e a
+anyOfNE (x:|xs) = foldl (<||>) x xs
+
+filterWith :: (a -> e) -> (a -> Bool) -> Parser i e a -> Parser i e a
+filterWith fail f p = lift $ \input ->
+    case run p input of
+        Right (a, input')
+            | f a -> Right (a, input')
+            | otherwise -> Left (fail a)
         Left e -> Left e
 
-match :: (Char -> Bool) -> Parser Char
-match f = next >>= \c ->
-    if f c
-    then pure c
-    else failWith "Failed to match Char"
+zeroOrMore :: Parser i e a -> Parser i Void [a]
+zeroOrMore p = lift $ \input ->
+    case run (oneOrMore p) input of
+        Right a -> Right a
+        Left e -> Right ([], input)
 
-option :: Parser a -> Parser (Maybe a)
-option p = Just <$> p <|> pure Nothing
+oneOrMore :: Parser i e a -> Parser i e [a]
+oneOrMore p = (:) <$> p <*> (oneOrMore p <||> pure [])
 
-eol :: Parser ()
-eol = oneOrMore carriageReturnNewLine $> ()
-  where
-    carriageReturnNewLine = match (`elem` ['\r', '\n'])
+pelem :: Eq a => [a] -> Parser [a] MatchError a
+pelem xs = match (`elem` xs)
 
-eof :: Parser ()
-eof = zeroOrMore eol &&. lift isEof
-  where
-    isEof :: ParseState -> ParseResult ()
-    isEof s = case source s of
-        [] -> Right ((), s)
-        _ -> Left $ toError "Expected EOF" s        
-
-pchar :: Char -> Parser Char
-pchar c = match (== c) <?> "Expected '" <> show c <> "'"
-
-pcharIn :: [Char] -> Parser Char
-pcharIn = match . flip elem
-
-pcharNotIn :: [Char] -> Parser Char
-pcharNotIn cs = match $ not . flip elem cs
-
-pdigit :: Parser Char
-pdigit = match isDigit <?> "Expected digit"
-
-pint :: Parser Int
-pint =
-    (bool id negate . isJust <$> option (pchar '-'))
-    >>= \f -> f . read <$> oneOrMore pdigit
-
-whitespace :: Parser Char
-whitespace = match (`elem` [' ', '\t', '\r', '\n']) <?> "Expected whitespace"
-
-pstr :: String -> Parser String
-pstr s = traverse pchar s <?> "Expected '" <> s <> "'"
-
-pquotedstr :: Parser String
-pquotedstr = quote &&. zeroOrMore nonQuote .&& quote <?> "Expected quoted string"
-  where
-    quote = pchar '"'
-    nonQuote = match (/= '"')
+option :: Parser i e a -> Parser i Void (Maybe a)
+option p = lift $ \input ->
+    case run p input of
+        Right r -> Right $ first Just r
+        Left _ -> Right (Nothing, input)
